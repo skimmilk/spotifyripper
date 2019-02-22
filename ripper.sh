959024a7711a72d7babe6c6bf1bceefc0806fd33
Panciz <dav.poletti@gmail.com>
2019-02-22 05:29:28 +0100

    allow to call the script from any filesystem path

--- ripper.sh
+++ ripper.sh
@@ -29,7 +29,7 @@ trap 'pactl move-sink-input $spotify $pasink' EXIT
 # Move Spotify to its own sink so recorded output will not get corrupted
 pactl move-sink-input $spotify spotify
 
-./notify.sh | while read line
+$(dirname $(readlink -f $0))/notify.sh | while read line
 do
   if [[ $line == "__SWITCH__" ]]; then
     killall oggenc 2>/dev/null
